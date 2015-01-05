c0db81256123719d995299f58a8a457ea57ed566
hellonico <hellonico@gmail.com>
2015-01-05 19:02:57 +0900

    perms

---
+++ src/maths/binaryperm.clj
@@ -0,0 +1,9 @@
+(ns maths.binaryperm)
+
+(defn binary-permutation [n]
+  (for [x (range (bit-shift-left 1 n))]
+    (apply str (take n (map #(bit-and 1 %) (iterate #(bit-shift-right % 1) x))))))
+
+(binary-permutation 5)
+
+
