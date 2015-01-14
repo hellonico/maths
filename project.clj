8b9c97368ef8e4c416f36e9b57d8d60833c61783
hellonico <hellonico@gmail.com>
2015-01-14 10:26:35 +0900

    remember the priority map

--- project.clj
+++ project.clj
@@ -6,16 +6,16 @@
   :jvm-opts ["-Xmx16g" "-server" "-XX:-UseGCOverheadLimit"]
   :profiles {
     :dev {:plugins [[lein-midje "3.0.0"]]
-          :dependencies [[primitive-math "0.1.4"]
-                         [org.clojure/tools.trace "0.7.8"]
+          :dependencies [[org.clojure/tools.trace "0.7.8"]
                          [midje "1.6.3"]
                          [org.clojure/tools.namespace "0.2.8"]
                          [criterium "0.4.3"]
-                         [clj-stacktrace "0.2.4"]]
-          }
+                         [clj-stacktrace "0.2.4"]]}
   }
   :dependencies [[org.clojure/data.priority-map "0.0.5"]
                  [net.mikera/core.matrix "0.32.1"]
+                 [primitive-math "0.1.4"]
+                 [org.clojure/data.priority-map "0.0.5"]
                  [net.mikera/vectorz-clj "0.28.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/clojure "1.7.0-alpha5"]])
