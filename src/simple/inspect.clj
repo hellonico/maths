66f4baa57df0a2ef648419986b8068c62af2d79e
Nicolas Modrzyk <hellonico@gmail.com>
2015-04-23 17:45:23 +0900

    comments

--- src/simple/inspect.clj
+++ src/simple/inspect.clj
@@ -1,6 +1,9 @@
 (ns simple.inspect
   (:require [clojure.inspector :refer :all]))
 
+; shows how you can inspect a map or a tree
+; graphically without any additional software
+; the below will start a new  jframe
 (def a {:a 1 :b 2 :c {:a 2 :b 1}})
 (inspect-tree a)
-; jframe starts
+
